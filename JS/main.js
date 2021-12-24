8a02a8803081c408e539cd197c0483133bc94537
ZohidovaRoziya <rzohidova8@gmail.com>
2021-12-24 09:53:42 +0500

    add mine project javasicript logica

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,47 @@
-let body_El = document.querySelector("body");
-let result_Btn_El = document.querySelector("#result");
-let heading_El = document.querySelector("#heading");
+let name_bmi_El = document.getElementById("name_bmi");
+let bmi_form_El = document.getElementById("bmi_form")
+let weight_bmi_El = document.getElementById("weight_bmi");
+let height_bmi_El = document.getElementById("height_bmi");
+let card_heading_El = document.getElementById("card_heading")
 
-let userName = prompt("Ismingizni Kiriting : ");
-let bmiName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase()
 
-let weight = prompt("Vaznigiz o'lchovini kiriting : ");
-let height = prompt("Boyingiz o'lchamini kiriting : ");
-let BMI =  weight / height  **2;
-
-result_Btn_El.addEventListener( "click" , function () {
-    if (BMI < 18.5) {
-        heading_El.textContent = `${bmiName} sizning vazningiz ozg'in`
-        body_El.style.backgroundColor = 'midnightblue'
-        result_Btn_El.style.display = "none"
-    }else if ( BMI > 18.5  && BMI < 24.9 ) {
-        heading_El.textContent = `${bmiName} sizning vazningiz normal`
-        body_El.style.backgroundColor = 'magenta'
-        result_Btn_El.style.display = "none"
+bmi_form.addEventListener( "submit", (e)=> {
+    e.preventDefault()
+    let name = name_bmi_El.value;
+    let bmiName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    let weight = weight_bmi_El.value;
+    let height = height_bmi_El.value;
+    let BMI =  (weight / height  **2).toFixed(1);
+    
+    if (BMI <= 18.5) {
+        card_heading_El.textContent = `${bmiName} siz o'zg'insiz !`
+    } else if (BMI > 18.5  && BMI < 24.9 ) {
+        card_heading_El.textContent = `${bmiName} siz normanlisiz!`
     } else if (BMI > 25  && BMI < 29.9) {
-        heading_El.textContent = `${bmiName} sizning vazningiz Semiz`
-        body_El.style.backgroundColor = 'red'
-        result_Btn_El.style.display = "none"
+        card_heading_El.textContent = `${bmiName} siz semizsiz!`
     } else {
-        heading_El.textContent = `${bmiName} sizning vazningiz o'ta Semiz`
-        body_El.style.backgroundColor = 'blue'
-        result_Btn_El.style.display = "none"
+        card_heading_El.textContent = `${bmiName} siz o'ta semisz!`
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
